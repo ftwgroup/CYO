6669cb436f4131765eea5bc87334d0e43e9cecdf
Ipsheeta Furtado <ipsheeta.furtado@gmail.com>
2011-11-14 14:09:37 -0800

    edited models and piece

--- repertoire/models.py
+++ repertoire/models.py
@@ -1,4 +1,6 @@
 from django.contrib.localflavor import us
+from django.contrib.localflavor.us.models import USStateField, USPostalCodeField
+
 from django.db import models
 
 from django.utils.translation import ugettext_lazy as _
@@ -8,12 +10,15 @@ from feincms.content.richtext.models import RichTextContent
 from feincms.content.image.models import ImageContent
 from feincms.content.medialibrary.v2 import MediaFileContent
 
+import os.path
+from settings import PROJECT_ROOT
+
 Page.register_extensions('datepublisher', 'navigation', 'seo', 'titles')
 
 Page.register_templates({
     'title': _('Upcoming'),
-    #TODO (julian) Needs to work on more than my computer
-    'path': '/Users/julian/projects/cyo/repertoire/templates/piece.html',
+    #TODO (ipsheeta) is this going to work on everyone's computer now?
+    'path': (os.path.join(PROJECT_ROOT, 'repertoire/templates/piece.html')),
     'regions': (
         ('yellow_overview', _('Overview')),
         ('white_details', _('Details')),
@@ -39,30 +44,8 @@ CONCERT_TYPES = (
     ('RO', 'Rock the Orchestra'),
 )
 
-class ConcertInfo(models.Model):
-    date_time = models.DateTimeField('Date and Time')
-    address1 = models.CharField(max_length=50)
-    address2 = models.CharField(max_length=50, blank=True, null=True)
-    city = models.CharField(max_length=50, blank=True, null=True)
-    #TODO (julian) these modules are not imported
-    #state = us.models.USStateField(blank=True, null=True)
-    #zipcode = us.models.USPostalCodeField(blank=True, null=True)
-
-    type_of_concert = models.CharField(max_length=50, choices=CONCERT_TYPES)
-    #TODO (julian) an error is thrown when defining this
-    #songlist = models.ManyToManyField(AllSongs)
-    soloist1 = models.CharField(max_length=50, blank=True, null=True)
-    soloist2 = models.CharField(max_length=50, blank=True, null=True)
-    guest = models.CharField(max_length=50, blank=True, null=True)
-
-    class Meta:
-        abstract = True
-
-    def render(self, *args, **kwargs):
-        return
-    
 class AllSongs(models.Model):
-    #TODO (julian) Check that this is defined right
+    #TODO (ipsheeta) I don't think I want this relationship defined yet
     #performance = models.ManyToManyField(ConcertInfo)
     title = models.CharField(max_length=50)
     composer = models.CharField(max_length=50)
@@ -73,3 +56,33 @@ class AllSongs(models.Model):
 
     def render(self, *args, **kwargs):
         return
+    #TODO (Ipsheeta) is this sufficient?
+    def __unicode__(self):
+        return self.title
+
+class ConcertInfo(models.Model):
+    date_time = models.DateTimeField('Date and Time')
+    address1 = models.CharField(max_length=50)
+    address2 = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    state = us.models.USStateField(blank=True, null=True)
+    zipcode = us.models.USPostalCodeField(blank=True, null=True)
+
+    headline = models.CharField(max_length=100)
+    type_of_concert = models.CharField(max_length=50, choices=CONCERT_TYPES)
+    #TODO (ipsheeta) still an error?
+    songlist = models.ForeignKey(AllSongs)
+    soloist1 = models.CharField(max_length=50, blank=True, null=True)
+    soloist2 = models.CharField(max_length=50, blank=True, null=True)
+    guest = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+    def render(self, *args, **kwargs):
+        return
+
+    #TODO (Ipsheeta) is this sufficient?
+    def __unicode__(self):
+        return self.headline
+    
