6a9c11a0df39ca30d262f4ea5a7c2d17f2af2c4c
Julian Threatt <porfirio84@gmail.com>
2011-11-15 15:51:16 -0800

    made concert type work!

---
+++ repertoire/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from repertoire.models import Song, ConcertInfo
+
+admin.site.register(Song)
+admin.site.register(ConcertInfo)
--- repertoire/models.py
+++ repertoire/models.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.contrib.localflavor import us
 from django.contrib.localflavor.us.models import USStateField, USPostalCodeField
 
 from django.db import models
+from django.template.loader import render_to_string
 
 from django.utils.translation import ugettext_lazy as _
 
@@ -9,6 +11,7 @@ from feincms.module.page.models import Page
 from feincms.content.richtext.models import RichTextContent
 from feincms.content.image.models import ImageContent
 from feincms.content.medialibrary.v2 import MediaFileContent
+from feincms import settings
 
 import os.path
 from settings import PROJECT_ROOT
@@ -44,18 +47,19 @@ CONCERT_TYPES = (
     ('RO', 'Rock the Orchestra'),
 )
 
-class AllSongs(models.Model):
+class Song(models.Model):
+    """
+    Song class for the song database. Includes the :class: 'feincms.models.ExtensionMixin'
+    because of the (handy) extension mechanism.
+    """
     #TODO (ipsheeta) I don't think I want this relationship defined yet
     #performance = models.ManyToManyField(ConcertInfo)
     title = models.CharField(max_length=50)
     composer = models.CharField(max_length=50)
     premiered_by = models.CharField(max_length=50, blank=True, null=True)
+    #TODO (julian) at some point they will want to upload the actual music for the kids
+    #file = models.FileField(_('sheet music'), max_length=255, upload_to=settings.FEINCMS_MEDIALIBRARY_UPLOAD_TO)
 
-    class Meta:
-        abstract = True
-
-    def render(self, *args, **kwargs):
-        return
     #TODO (Ipsheeta) is this sufficient?
     def __unicode__(self):
         return self.title
@@ -71,18 +75,29 @@ class ConcertInfo(models.Model):
     headline = models.CharField(max_length=100)
     type_of_concert = models.CharField(max_length=50, choices=CONCERT_TYPES)
     #TODO (ipsheeta) still an error?
-    songlist = models.ForeignKey(AllSongs)
+    songs = models.ManyToManyField(Song)
     soloist1 = models.CharField(max_length=50, blank=True, null=True)
     soloist2 = models.CharField(max_length=50, blank=True, null=True)
     guest = models.CharField(max_length=50, blank=True, null=True)
 
-    class Meta:
-        abstract = True
-
-    def render(self, *args, **kwargs):
-        return
-
     #TODO (Ipsheeta) is this sufficient?
     def __unicode__(self):
         return self.headline
-    
+
+class ContentWithConcertInfo(models.Model):
+    """
+    This is the content type for the concert info
+    """
+    concert = models.ForeignKey(ConcertInfo, verbose_name=_('concert info'))
+
+    class Meta:
+        abstract = True
+
+    def render(self, **kwargs):
+        ctx = {'content', self}
+        ctx.update(kwargs)
+
+        #TODO (julian) Needs a template file
+        return render_to_string('concert_info.html', ctx)
+
+Page.create_content_type(ContentWithConcertInfo)
