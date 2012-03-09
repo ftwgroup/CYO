7f3c5bbcdec42e7d48434d2286fe78163286ee90
Stephen H <stephenhan@gmail.com>
2012-03-08 19:01:04 -0800

    added breadcrumbs to templates     added correct links to social media icons     added tertiary navigation for pages within a page within a page     added blockquote  to fix alumni spotlight spacing     Fixed Liza spacing     Removed user agent styling for lists (it was showing up in different broswers)     Tweaked CSS for headings to work with breadcrumbs     Added JS and CSS for breadcrumbs customization

---
+++ staticfiles/js/fxns.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+/*Concert stuff*/
+    $('div.image').prependTo('div.event-details');
+    $(".zebra tr").mouseover(function(){$(this).addClass("over");}).mouseout(function(){$(this).removeClass("over");});
+    $(".zebra tr:even").addClass("alt");
+
+/*Bios Accordion Stuff*/
+    $('#sidebar-bios h3').next('p').hide();
+    makeSidebarOpenNext();
+
+/*Breadcrumbs*/
+    addSignagetoBreadcumbs()
+
+/*Document functions*/
+function makeSidebarOpenNext(){
+    $('#sidebar-bios h3').click(function () {
+        $(this).next().slideToggle('fast');
+    });
+}
+function addSignagetoBreadcumbs(){
+    if ($('ul.breadcrumbs').children().length < 2) {
+        $('ul.breadcrumbs').css('display', 'none');
+    }
+    $('<span>&gt;</span>').prependTo('ul.breadcrumbs > li:not(:first-child)');
+}
+});/*The end*/
