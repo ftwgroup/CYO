631c3f2a88b5965db6b342216137fae8c73bd068
Julian Threatt <porfirio84@gmail.com>
2012-04-20 06:36:35 -0700

    tinymce jquery plugin integrated into the concerts model. Can be extended into other models and admin views as well.

--- repertoire/admin.py
+++ repertoire/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from models import *
+from repertoire.forms import ConcertForm
 
-admin.site.register(Concert)
+class ConcertAdmin(admin.ModelAdmin):
+    form = ConcertForm
+
+admin.site.register(Concert, ConcertAdmin)
 admin.site.register(PerformedSong)
 admin.site.register(Performer)
 admin.site.register(Series)
