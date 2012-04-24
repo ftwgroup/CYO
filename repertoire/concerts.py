71ca58ad686e39d6272851fb4825812396f576e1
Jordan Carlson <jwgcarlson@gmail.com>
2012-04-24 14:19:08 -0700

    Added ConcertInSeriesView and template.

--- repertoire/concerts.py
+++ repertoire/concerts.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from feincms.views.generic.list_detail import object_list
@@ -25,6 +27,38 @@ class ConcertDetailView(DetailView):
     template_name = 'concert_detail.html'
 
 
+class ConcertInSeriesView(DetailView):
+    """
+    View details for a particular season of a particular concert series.
+    """
+    context_object_name = 'concert'
+    template_name = 'concert_in_series.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # TODO (jordan): error checking
+        self.series = kwargs.get('series', None)
+        self.season = int(kwargs.get('season', 0))
+        return super(ConcertInSeriesView, self).dispatch(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        """
+        Return the concert for the specified season of the specified series.
+        """
+        try:
+            obj =  Concert.objects.get(series__title=self.series, season=self.season)
+        except ObjectDoesNotExist:
+            raise Http404(u"No concerts found matching the query")
+        return obj
+
+    def get_context_data(self, **kwargs):
+        # Get default context object
+        context = super(ConcertInSeriesView, self).get_context_data(**kwargs)
+        # Attach the next upcoming concert to the context object
+        context['upcoming_in_series'] =\
+            Concert.objects.filter(series__title=self.series).latest('date_time')
+        return context
+
+
 """
 Setting URL patterns here.
 
@@ -37,6 +71,20 @@ urlpatterns = patterns('',
     # by default, these views show archive view using.
     # views are set to show objects filtered by series title and then ordered by date_time
     # may have to refactor to get url and then pass into filter to account for series other than the main three
+
+    # View concert details within a specific series
+    url(r'new-works/(?P<season>\d+)/$',
+        ConcertInSeriesView.as_view(),
+        {'series': 'New Works'},
+        name='new-works-detail'),
+    url(r'rock-the-orchestra/(?P<season>\d+)/$',
+        ConcertInSeriesView.as_view(),
+        {'series': 'Rock the Orchestra'},
+        name='rock-the-orchestra-detail'),
+    url(r'music-and-its-industry/(?P<season>\d+)/$',
+        ConcertInSeriesView.as_view(),
+        {'series': 'Music and Its Industry'},
+        name='music-and-its-industry-detail'),
 
     url(r'new-works/$', ListView.as_view(queryset=Concert.objects.filter(series__title="New Works").order_by('-season'), template_name='concert_archive.html'),
         name='new-works'),
