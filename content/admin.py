d53355c480e0c7168d68f16da0e1a5adfd1aa590
Stephen <stephenhan@gmail.com>
2012-05-31 12:25:21 -0700

    added a dir for fixtures

--- content/admin.py
+++ content/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
 from models import FeaturedBoxContent
+from models import FeaturedBoxContent
+
+class FeaturedBoxAdmin(admin.ModelAdmin):
+    raw_id_fields = ("poster_thumbnail",)
+
+admin.site.register(FeaturedBoxContent, FeaturedBoxAdmin)
