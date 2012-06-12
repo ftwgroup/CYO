7f2cf4577f6c8dac5923696451ae058311db3257
Stephen <stephenhan@gmail.com>
2012-06-11 21:25:58 -0700

    removed two column text page model

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ from feincms.module.page.models import Page
 from feincms.module.page.sitemap import PageSitemap
 from django.contrib import admin
 from django.views.generic.simple import redirect_to
+from content.views import BecomeADonor, BecomeASponsor, ProgramAds
 
 
 admin.autodiscover()
@@ -16,10 +17,14 @@ urlpatterns = patterns('',
     # url(r'^$', 'cyo.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^audition/', include('auditions.urls')),
+    #url(r'^become-a-donor/', BecomeADonor.as_view()),
+    #url(r'^become-a-sponsor/', BecomeASponsor.as_view()),
+    #url(r'^buy-program-ads/', ProgramAds.as_view()),
     url(r'^repertoire/', include('repertoire.urls')),
     #url(r'^concert/$', redirect_to, {'url' : '/concerts/'}),
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
             {'sitemaps': sitemaps}),
+    url(r'^', include('content.urls')),
     url(r'^', include('feincms.urls')),
 )
 handler404 = 'content.views.page_not_found'
