178b120c4fc44b15b1abcfc2dc9235313392fede
mo <mo@ftwgroup.com>
2012-11-29 17:45:36 -0800

    made 'search' vanish on click

--- staticfiles/js/generic.js
+++ staticfiles/js/generic.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
-    $("a.image_wrapped, a.image_downloadable").fancybox({
-        openEffect: 'elastic',
-        closeEffect: 'elastic',
-        prevEffect	: 'none',
-        nextEffect	: 'none',
-        maxWidth : 640,
-        maxHeight : 480,
-        closeBtn		: true,
-        helpers	: {
-            title	: {
-                type: 'inside'
-            },
-            overlay	: {
-                opacity : 0.8,
-                css : {
-                    'background-color' : '#000'
+        $("a.image_wrapped, a.image_downloadable").fancybox({
+            openEffect: 'elastic',
+            closeEffect: 'elastic',
+            prevEffect	: 'none',
+            nextEffect	: 'none',
+            maxWidth : 640,
+            maxHeight : 480,
+            closeBtn		: true,
+            helpers	: {
+                title	: {
+                    type: 'inside'
+                },
+                overlay	: {
+                    opacity : 0.8,
+                    css : {
+                        'background-color' : '#000'
+                    }
+                },
+                thumbs	: {
+                    width	: 50,
+                    height	: 50
                 }
-            },
-            thumbs	: {
-                width	: 50,
-                height	: 50
             }
-        }
-    });
-});
+        });
+//        Make 'search' vanish
+        $("input[name='q']").focus(function(){
+            if (this.value=='Search') this.value='';
+        }).blur(function(){
+            if (this.value=='') this.value='Search';
+        });
+    }
+);
